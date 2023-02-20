4a6031b5ba9bbbe77cc882f67aedfcc1070ab4ba
Ilbert130 <ilbertca27@gmail.com>
2023-02-21 04:16:35 +0800

    server added and database

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -20,4 +19,3 @@ document.querySelector('#app').innerHTML = `
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
