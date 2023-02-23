91405ebfe7650434f0a5da06f7359c9a25a6d06d
Ilbert130 <ilbertca27@gmail.com>
2023-02-23 12:56:18 +0800

    delete fixed

--- src/users/presentation/render-table/render-table.js
+++ src/users/presentation/render-table/render-table.js
@@ -1,5 +1,6 @@
 import usersStore from '../../store/users-store';
 import { showModal } from '../render-modal/render-modal.js';
+import {deleteUserById} from '../../use-cases/delete-user-by-id'
 import './render-table.css';
 
 let table;
@@ -39,6 +40,29 @@ const tableSelectListener = (event) => {
 
 /**
  * 
+ * @param {MauseEvent} event 
+ */
+const tableDeleteListener = async (event) => {
+
+    const element = event.target.closest('.delete-user');
+    if(!element) return;
+
+    const id = element.getAttribute('data-id');
+    
+    try{
+        await deleteUserById(id);
+        await usersStore.reloadPage();
+        document.querySelector('#current-page').innerText = usersStore.getCurrentPage();
+        renderTable();
+
+    }catch (error){
+        console.log(error);
+        alert('No se pudo eliminar');
+    }
+}
+
+/**
+ * 
  * @param {HTMLDivElement} element 
  */ 
 export const renderTable = (element) => {
@@ -49,8 +73,8 @@ export const renderTable = (element) => {
         table = createTable();
         element.append(table);
 
-        //TODO: listeners a la tabla
         table.addEventListener('click', event => tableSelectListener(event));
+        table.addEventListener('click', event => tableDeleteListener(event));
     }
 
     let tableHTML = '';
